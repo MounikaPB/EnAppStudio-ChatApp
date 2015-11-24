2941457b29ebb48bf501e8934238a4a95de042f8
Mounika Potula Boguda <Mounika@knuts-imac.local>
2015-11-24 11:43:43 +0100

    Initial Commit

---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Mounika Potula Boguda on 2015-11-24.
+//  Copyright © 2015 En Appstudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
